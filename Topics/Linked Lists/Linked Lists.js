2679136625a078629821330dbc91931431659ccb
godisblaaack <163236412+godisblaack@users.noreply.github.com.>
2024-07-17 11:06:11 +0530

    Updated the code of Linked List Initialization

--- Topics/Linked Lists/Linked Lists.js
+++ Topics/Linked Lists/Linked Lists.js
@@ -22,7 +22,7 @@ import java.util.LinkedList;
 
 public class LinkedListsInitialization {
   public static void main(String[] args) {
-    LinkedList list = new LinkedList();
+    LinkedList<Integer> list = new LinkedList<>();
 
     list.addLast(10);
     list.addLast(20);
@@ -52,6 +52,46 @@ public class LinkedListsInitialization {
     System.out.println(Arrays.toString(array));
   }
 }
+
+/*
+  Warning: LinkedList is a raw type. References to generic type LinkedList<E> should be parameterized.
+
+  import java.util.Arrays;
+  import java.util.LinkedList;
+
+  public class LinkedListsInitialization {
+    public static void main(String[] args) {
+      LinkedList list = new LinkedList();
+
+      list.addLast(10);
+      list.addLast(20);
+      list.addLast(30);
+      list.addLast(40);
+      list.addLast(50);
+      System.out.println(list);
+
+      list.addFirst(5);
+      System.out.println(list);
+      
+      list.remove(1);
+      System.out.println(list);
+      
+      list.removeFirst();
+      System.out.println(list);
+
+      System.out.println(list.contains(30));
+
+      System.out.println(list.indexOf(20));
+
+      System.out.println(list.size());
+
+      System.out.println(list);
+
+      var array = list.toArray();
+      System.out.println(Arrays.toString(array));
+    }
+  }
+ */
 `,`
 import java.util.Arrays;
 import java.util.NoSuchElementException;
